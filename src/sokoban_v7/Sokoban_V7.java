cc28067da621e1ae916a08cceb4fc7f0fa71df13
20024094 <20024094@10.5.148.30>
2024-03-28 10:45:03 +0000

    Version 7

---
+++ src/sokoban_v7/Sokoban_V7.java
@@ -0,0 +1,16 @@
+package sokoban_v7;
+
+/**
+ *
+ * @author 20024094
+ */
+public class Sokoban_V7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
